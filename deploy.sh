146f2833bb6165d2575248de3f998cbcb4f83112
engvuchen <1742284391@qq.com>
2023-05-01 07:30:13 +0800

    auto deploy

--- deploy.sh
+++ deploy.sh
@@ -8,19 +8,19 @@ set -e
 npm run docs:build
 
 # navigate into the build output directory
-cd docs/.vitepress/dist
+# cd docs/.vitepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'auto deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 # just a example, please change the follow line with your github account!!!
- git push -f https://github.com/cunzaizhuyi/cunzaizhuyi.github.io.git master
+git push -f https://github.com/engvuchen/blog.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
