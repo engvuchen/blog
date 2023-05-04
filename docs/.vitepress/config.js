743a1ff44942210bfa939fe46ed81d4c85a69f8d
engvuchen <1742284391@qq.com>
2023-05-05 01:58:57 +0800

    U 去掉dist/生产环境支持又拍云链接

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -25,9 +25,14 @@ export default defineConfig({
     ],
 
     footer: {
-      message: ['Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.'],
+      message: 'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
       copyright: [
-        'Copyright ©2023-present <a href="https://github.com/engvuchen">Engvuchen</a><br><div style="display: flex; justify-content: center; align-items: center;">本网站由 <a style="display: inline-block; line-height: 0;" href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral"><img style="width: 60px; display: inline-block" alt="cloud.png" src="../cloud.png"/></a> 提供 CDN 加速/云存储服务</div>',
+        `Copyright ©2023-present <a href="https://github.com/engvuchen">Engvuchen</a>
+        <span style="display: flex; justify-content: center; align-items: center;">本网站由
+          <a style="display: inline-block; line-height: 0;" href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral">
+            <img style="width: 60px; display: inline-block" alt="cloud.png" src="../cloud.png">
+          </a> 提供 CDN 加速/云存储服务
+        </span>`,
       ],
     },
 
